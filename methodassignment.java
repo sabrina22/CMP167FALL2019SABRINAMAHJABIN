79d6343bd94d60457f3d33c4715327b69219437b
sabrina22 <37752666+sabrina22@users.noreply.github.com>
2019-10-10 17:00:30 -0400

    Add files via upload

---
+++ methodassignment.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @author Sabrina Mahjabin
+ *
+ */
+
+public class methodassignment {
+
+	public static int productOfValues(int value1,int value2,int value3) {
+		return value1 * value2 * value3;
+	}
+	public static int sumOfValues(int value1,int value2) {
+		int sum = 0;
+		for (int i = value1; i<=value2; i++) {
+			sum = (sum + i); 
+		}
+		return sum;
+	}
+	
+	public static int averageOfValues(int n) {
+		int sum = 0;
+		for (int p=1; p ) {
+			sum = (sum + p);
+		}
+	}
+}
